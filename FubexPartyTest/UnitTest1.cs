f2c9ed9808452ed9154e3add6bd3552e82e41c3f
thedrops <pedrops02@gmail.com>
2016-05-30 17:20:23 -0300

    FubexParty

---
+++ FubexPartyTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FubexPartyTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

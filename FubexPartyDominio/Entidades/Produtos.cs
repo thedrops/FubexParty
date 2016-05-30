397dadcf865234c498d714d13c1cdbd4743c4e65
thedrops <pedrops02@gmail.com>
2016-05-30 17:51:23 -0300

    TheDrops

---
+++ FubexPartyDominio/Entidades/Produtos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FubexPartyDominio.Entidades
+{
+    class Produtos
+    {
+        public int ProdutoID { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public string Categoria { get; set; }
+
+
+    }
+}
